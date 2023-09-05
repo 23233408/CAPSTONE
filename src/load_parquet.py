2a7909580d3ef789f4ccdf0c39fa17ef204b1862
Kha Huynh <23233408@student.uwa.edu.au>
2023-09-05 13:29:38 +0800

    Update load parameters

--- src/load_parquet.py
+++ src/load_parquet.py
@@ -3,14 +3,11 @@ import dask.dataframe as dd
 import pandas as pd
 from datetime import datetime
 
-def load_labevents_dask(ROOT_DIR):
-    output_dir = ROOT_DIR / 'data' / 'labevents_transform_parquet'
-    file_name = 'labevents_transform'
-
+def load_labevents_dask(file_dir):
     print(str(datetime.now()) + ' Start')
 
     # Read all Parquet files and concatenate the list of DataFrames into a single DataFrame
-    ddf = dd.read_parquet(path=f'{output_dir}/*.parquet',
+    ddf = dd.read_parquet(path=f'{file_dir}/*.parquet',
                         index='row_key',
                         engine='fastparquet')
 
@@ -24,12 +21,9 @@ def load_labevents_dask(ROOT_DIR):
     print(str(datetime.now()) + ' End')
     return ddf
 
-def load_labevents_pd(ROOT_DIR):
-    output_dir = ROOT_DIR / 'data' / 'labevents_transform_sepsis_parquet'
-    file_name = 'labevents_transform_sepsis'
-
+def load_labevents_pd(dir):
     # List all Parquet files in the output folder
-    parquet_files = sorted([f'{output_dir}/{file}' for file in os.listdir(output_dir) if file.endswith('.parquet')])
+    parquet_files = sorted([f'{dir}/{file}' for file in os.listdir(dir) if file.endswith('.parquet')])
 
     print(str(datetime.now()) + ' Start')
 
@@ -37,13 +31,15 @@ def load_labevents_pd(ROOT_DIR):
     df = None
     for file in parquet_files:
         df_i = pd.read_parquet(file, 'fastparquet')
-        print(str(datetime.now()) + ' ' + file)
+        # print(str(datetime.now()) + ' ' + file)
         if df is None:
             df = df_i
         else:
             df = pd.concat([df, df_i], ignore_index = True)
+    
+    df.columns = df.columns.str.upper()
+
+    print(str(datetime.now()) + ' ' + str(len(df)))
 
     print(str(datetime.now()) + ' End')
-
-    print(len(df))
     return df
