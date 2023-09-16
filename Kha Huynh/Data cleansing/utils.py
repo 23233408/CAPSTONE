e0190947afc45cacce1c277757f91cfb5ea83dde
Kha Huynh <23233408@student.uwa.edu.au>
2023-09-17 01:42:24 +0800

    Create  date processing pipline

--- Kha Huynh/Data cleansing/utils.py
+++ Kha Huynh/Data cleansing/utils.py
@@ -20,3 +20,25 @@ def create_labevent_columns(df, item_ids):
     columns= np.append(columns, flag_columns)
     df = pd.concat([df, pd.DataFrame(np.nan, columns=columns, index=df.index)], axis=1)
     return df
+
+def categorise_age(df):
+    """
+    Categorise the 'AGE' column in the DataFrame into age bins.
+    
+    Parameters:
+    - df: DataFrame containing the 'AGE' column
+    
+    Returns:
+    - DataFrame with an additional 'AGE_cat' column containing the age categories
+    """
+
+    # define age bin edges
+    age_bin_edges = [0, 1, 18, 36, 51, 71, 121]
+    
+    # define bin labels: neonatal, child, adult, middle-age, elderly, very old
+    age_bin_labels = ['0', '1-17', '18-35', '36-50', '51-70', '71+']
+    
+    # create age bins
+    df['AGE_cat'] = pd.cut(df['AGE'], bins=age_bin_edges, labels=age_bin_labels, right=False)
+    
+    return df
