9c09c745f9833aaf7c158ee1f5c9abb5a05e7f23
alexrturner <50839467+alexrturner@users.noreply.github.com>
2023-09-16 17:45:49 +0800

    merged model pipeline

---
+++ AlexTurner/reference/simple-impute.py
@@ -0,0 +1,43 @@
+# https://github.com/MLforHealth/MIMIC_Extract/blob/455a2484c1fd2de3809ec2aa52897717379dc1b7/utils/simple_impute.py
+
+import copy
+import math
+import os
+import pickle
+import time
+import pandas as pd
+import numpy as np
+
+ID_COLS = ['subject_id', 'hadm_id', 'icustay_id']
+
+
+def simple_imputer(df, train_subj):
+    idx = pd.IndexSlice
+    df = df.copy()
+
+    df_out = df.loc[:, idx[:, ['mean', 'count']]]
+    icustay_means = df_out.loc[:, idx[:, 'mean']].groupby(ID_COLS).mean()
+    global_means = df_out.loc[idx[train_subj, :], idx[:, 'mean']].mean(axis=0)
+
+    df_out.loc[:, idx[:, 'mean']] = df_out.loc[:, idx[:, 'mean']].groupby(ID_COLS).fillna(
+        method='ffill'
+    ).groupby(ID_COLS).fillna(icustay_means).fillna(global_means)
+
+    df_out.loc[:, idx[:, 'count']] = (
+        df.loc[:, idx[:, 'count']] > 0).astype(float)
+    df_out.rename(columns={'count': 'mask'},
+                  level='Aggregation Function', inplace=True)
+
+    is_absent = (1 - df_out.loc[:, idx[:, 'mask']])
+    hours_of_absence = is_absent.cumsum()
+    time_since_measured = hours_of_absence - \
+        hours_of_absence[is_absent == 0].fillna(method='ffill')
+    time_since_measured.rename(
+        columns={'mask': 'time_since_measured'}, level='Aggregation Function', inplace=True)
+
+    df_out = pd.concat((df_out, time_since_measured), axis=1)
+    df_out.loc[:, idx[:, 'time_since_measured']] = df_out.loc[:,
+                                                              idx[:, 'time_since_measured']].fillna(100)
+
+    df_out.sort_index(axis=1, inplace=True)
+    return df_out
---
+++ AlexTurner/rph.sh
@@ -0,0 +1,2 @@
+ssh -L localhost:5000:localhost:5000 alex@146.118.68.235
+
